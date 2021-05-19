92e580606c005a049ab41ba2b775b683344500c8
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-05-19 19:03:54 +0200

    Bedre sjekk på at vi sitter med et gyldig RutingGrunnlag.

--- app/src/main/kotlin/no/nav/punsjbolle/ruting/RutingGrunnlag.kt
+++ app/src/main/kotlin/no/nav/punsjbolle/ruting/RutingGrunnlag.kt
@@ -4,6 +4,7 @@ internal data class RutingGrunnlag(
     internal val søker: Boolean? = null,
     internal val pleietrengende: Boolean? = null,
     internal val annenPart: Boolean? = null) {
-    init { require(søker != null || pleietrengende != null || annenPart != null) {"Minst et part må være evaluert."} }
+    private val ingenParter = søker == false && pleietrengende == false && annenPart == false
     internal val minstEnPart = søker == true || pleietrengende == true || annenPart == true
+    init { require(minstEnPart || ingenParter) {"Minst en part må være true, eller alle false. Var $this"} }
 }
