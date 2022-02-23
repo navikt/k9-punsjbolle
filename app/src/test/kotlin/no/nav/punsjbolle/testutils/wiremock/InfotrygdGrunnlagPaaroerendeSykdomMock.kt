760ccb851e03f39348f749ab08c35c032b783feb
albrektsson <emil.albrektsson@nav.no>
2022-02-23 13:35:00 +0100

    Mock-fiks

--- app/src/test/kotlin/no/nav/punsjbolle/testutils/wiremock/InfotrygdGrunnlagPaaroerendeSykdomMock.kt
+++ app/src/test/kotlin/no/nav/punsjbolle/testutils/wiremock/InfotrygdGrunnlagPaaroerendeSykdomMock.kt
@@ -2,10 +2,12 @@ package no.nav.punsjbolle.testutils.wiremock
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.http.Body
 import com.github.tomakehurst.wiremock.matching.AnythingPattern
 import no.nav.punsjbolle.testutils.wiremock.WireMockVerktøy.withAuthorizationHeader
 import no.nav.punsjbolle.testutils.wiremock.WireMockVerktøy.withNavGetHeaders
 import no.nav.punsjbolle.testutils.wiremock.WireMockVerktøy.withJson
+import no.nav.punsjbolle.testutils.wiremock.WireMockVerktøy.withNavPostHeaders
 
 private const val path = "/infotrygd-grunnlag-paaroerende-sykdom-mock"
 
@@ -18,16 +20,16 @@ private fun WireMockServer.mockPingUrl(): WireMockServer {
 
 private fun WireMockServer.mockSaker(): WireMockServer {
     WireMock.stubFor(
-        WireMock.get(WireMock.urlPathMatching(".*$path/saker.*"))
-            .withNavGetHeaders().withQueryParam("fnr", AnythingPattern()).withQueryParam("fom", AnythingPattern())
+        WireMock.post(WireMock.urlPathMatching(".*$path/saker.*"))
+            .withNavPostHeaders().withRequestBody(AnythingPattern())
             .willReturn(WireMock.aResponse().withJson("""{"saker":[], "vedtak":[]}""")))
     return this
 }
 
 private fun WireMockServer.mockVedtakForPleietrengende(): WireMockServer {
     WireMock.stubFor(
-        WireMock.get(WireMock.urlPathMatching(".*$path/vedtakForPleietrengende.*"))
-            .withNavGetHeaders().withQueryParam("fnr", AnythingPattern()).withQueryParam("fom", AnythingPattern())
+        WireMock.post(WireMock.urlPathMatching(".*$path/vedtakForPleietrengende.*"))
+            .withNavPostHeaders().withRequestBody(AnythingPattern())
             .willReturn(WireMock.aResponse().withJson("""[{"vedtak":[]}]""")))
     return this
 }
