261c433e88ae14c18b5b85245a66967b607609cc
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-07-01 12:01:35 +0200

    Revert "Bump the gradle group across 1 directory with 8 updates (#258)" (#259)
    This reverts commit 3b6973b4053a00395ce4f85cedc81e7807edaa0b.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val junitJupiterVersion = "5.10.3"
 val k9rapidVersion = "1.20240510083323-9f05ca1"
-val dusseldorfVersion = "5.0.2"
+val dusseldorfVersion = "5.0.1"
 val ktorVersion = "2.3.12"
-val jsonassertVersion = "1.5.3"
+val jsonassertVersion = "1.5.2"
 val mockkVersion = "1.13.11"
 val assertjVersion = "3.25.1"
 val k9SakKontraktVersion = "4.1.15"
